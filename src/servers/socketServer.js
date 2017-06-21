486d305901b2e0f12e6586280c0f9fa9d6457589
Jamie Copeland <Jamie.Copeland@thetrainline.com>
2017-06-21 10:17:35 +0100

    Fix socket server

--- src/servers/socketServer.js
+++ src/servers/socketServer.js
@@ -2,8 +2,9 @@ import createSocketServer from 'socket.io';
 
 const port = 3003;
 
-createSocketServer(port)
-  .on('connection', socket => socket
-    .emit('message', { userName: 'Mr Server',  body: 'Welcome to the chat' })
-    .on('message', message => socket.emit('message', message))
-  );
+const server = createSocketServer(port);
+
+server.on('connection', socket => socket
+  .emit('message', { userName: 'Mr Server',  body: 'Welcome to the chat' })
+  .on('message', message => server.emit('message', message))
+);
